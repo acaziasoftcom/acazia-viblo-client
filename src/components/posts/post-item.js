72a2b33e043bcb56a1028f2a43948720974b6366
Truong <truongbk227@gmail.com>
2018-05-30 21:04:24 +0700

    update icon

--- src/components/posts/post-item.js
+++ src/components/posts/post-item.js
@@ -1,79 +1,95 @@
 import React, { Component } from 'react';
 import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
-import { Icon } from 'react-native-elements'
-const Item = ({ icon, count, style }) => {
-    return (
-        <View style={[{ flexDirection: 'row' }, style]}>
-            <Icon
-                name={icon}
-                type='material-community'
-                color='#5E5EF4'
-                size={17}
-            />
-            <Text style={{ marginLeft: 5 }}>{count}</Text>
-        </View>
-    )
-}
+import { Icon } from 'react-native-elements';
+const Item = ({ icon, count, style, type }) => {
+  return (
+    <View style={[{ flexDirection: 'row' }, style]}>
+      <Icon name={icon} type={type} color="#71726A" size={20} />
+      <Text style={{ marginLeft: 5 }}>{count}</Text>
+    </View>
+  );
+};
+
 const icons = [
-    {
-        id: 1,
-        icon: 'eye',
-        count: 0
-    },
-    {
-        id: 2,
-        icon: 'eye',
-        count: 0
-    },
-    {
-        id: 3,
-        icon: 'comment-multiple',
-        count: 0
-    },
-    {
-        id: 4,
-        icon: 'eject',
-        count: 0
-    }
-]
+  {
+    id: 1,
+    icon: 'eye',
+    count: 0,
+    type: 'font-awesome'
+  },
+  {
+    id: 2,
+    icon: 'paperclip',
+    count: 0,
+    type: 'font-awesome'
+  },
+  {
+    id: 3,
+    icon: 'comments',
+    count: 0,
+    type: 'font-awesome'
+  },
+  {
+    id: 4,
+    icon: 'sort',
+    count: 0,
+    type: 'font-awesome'
+  }
+];
 export default class PostItem extends Component {
-    render() {
-        return (
-            <TouchableOpacity style={{ backgroundColor: '#fff', marginBottom: 5, paddingLeft: 10, paddingTop: 4, paddingBottom: 4 }}>
-                <View style={styles.conainerHeader}>
-                    <Image
-                        style={{ width: 45, height: 45, borderRadius: 23 }}
-                        source={{ uri: 'http://i.9mobi.vn/cf/images/2015/03/nkk/nhung-hinh-anh-dep-3.jpg' }}
-                    />
-                    <View style={{ marginLeft: 10 }}>
-                        <Text style={{ fontSize: 13 }}>Trương bk hn</Text>
-                        <Text style={{ fontSize: 12 }}>Trương bk hn</Text>
-                    </View>
-                </View>
-                <Text style={styles.title}>Trương pro vip bn vd</Text>
-                <View style={styles.containerIcon}>
-                    {icons.map((value) => {
-                        if (value.id !== 1) {
-                            return (
-                                <Item style={{ marginLeft: 20 }} icon={value.icon} count={value.count} />
-                            )
-                        } else {
-                            return (
-                                <Item icon={value.icon} count={value.count} />
-                            )
-                        }
-                    })}
-                </View>
-                <Text>ssssssssssss</Text>
-            </TouchableOpacity>
-        );
-    }
+  render() {
+    return (
+      <TouchableOpacity
+        style={{
+          backgroundColor: '#fff',
+          marginBottom: 5,
+          paddingLeft: 10,
+          paddingTop: 4,
+          paddingBottom: 4
+        }}
+      >
+        <View style={styles.conainerHeader}>
+          <Image
+            style={{ width: 45, height: 45, borderRadius: 23 }}
+            source={{
+              uri:
+                'http://i.9mobi.vn/cf/images/2015/03/nkk/nhung-hinh-anh-dep-3.jpg'
+            }}
+          />
+          <View style={{ marginLeft: 10 }}>
+            <Text style={{ fontSize: 13 }}>Trương bk hn</Text>
+            <Text style={{ fontSize: 12 }}>Trương bk hn</Text>
+          </View>
+        </View>
+        <Text style={styles.title}>Trương pro vip bn vd</Text>
+        <View style={styles.containerIcon}>
+          {icons.map(value => {
+            if (value.id !== 1) {
+              return (
+                <Item
+                  style={{ marginLeft: 20 }}
+                  icon={value.icon}
+                  count={value.count}
+                  type={value.type}
+                />
+              );
+            } else {
+              return (
+                <Item icon={value.icon} count={value.count} type={value.type} />
+              );
+            }
+          })}
+        </View>
+        <Text>ssssssssssss</Text>
+      </TouchableOpacity>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    conainerHeader: {
-        flexDirection: 'row',
-    },
-    title: { fontSize: 18, color: '#000', marginBottom: 12, marginTop: 5 },
-    containerIcon:{ flexDirection: 'row', marginBottom: 17 }
+  conainerHeader: {
+    flexDirection: 'row'
+  },
+  title: { fontSize: 18, color: '#000', marginBottom: 12, marginTop: 5 },
+  containerIcon: { flexDirection: 'row', marginBottom: 17 }
 });
--- src/screens/stack-based/posts.js
+++ src/screens/stack-based/posts.js
@@ -1,60 +1,62 @@
-import React, { Component } from 'react'
-import {
-    View, Text, ScrollView, Dimensions
-} from 'react-native';
+import React, { Component } from 'react';;
+import { View, Text, ScrollView, Dimensions } from 'react-native';
 import PostItem from '../../components/posts/post-item';
 import { TabViewAnimated, TabBar, SceneMap } from 'react-native-tab-view';
 const list = [1, 1, 1, 1, 1, 1];
 const Sence = () => {
-    return (
-        <ScrollView style={{ backgroundColor: '#F1F2EC', marginTop: 3 }}>
-            {list.map(value => {
-                return (
-                    <PostItem key={Math.random()} />
-                )
-            })}
-        </ScrollView>
-    )
-}
+  return (
+    <ScrollView style={{ backgroundColor: '#F1F2EC', marginTop: 3 }}>
+      {list.map(value => {
+        return <PostItem key={Math.random()} />;
+      })}
+    </ScrollView>
+  );
+};
 
 const initialLayout = {
-    height: 0,
-    width: Dimensions.get('window').width,
-  };
+  height: 0,
+  width: Dimensions.get('window').width
+};
 export default class Post extends React.Component {
-    // static navigationOptions = () => ({
-    //     header: null
-    // });
-    constructor(props) {
-        super(props);
-        this.state = {
-            index: 0,
-            routes: [
-                { key: 'first', title: 'Newest' },
-                { key: 'second', title: 'Editors choice'  },
-                { key: 'thirty', title: 'Trending' },
-            ],
-        };
-    }
+  // static navigationOptions = () => ({
+  //     header: null
+  // });
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: 0,
+      routes: [
+        { key: 'first', title: 'Newest' },
+        { key: 'second', title: 'Editors choice' },
+        { key: 'thirty', title: 'Trending' }
+      ]
+    };
+  }
 
-    _handleIndexChange = index => this.setState({ index });
+  _handleIndexChange = index => this.setState({ index });
 
-    _renderHeader = props => <TabBar {...props} labelStyle={{fontSize: 13, color:'red'}} />;
+  _renderHeader = props => (
+    <TabBar
+      {...props}
+      indicatorStyle={{ backgroundColor: '#fff' }}
+      labelStyle={{ fontSize: 13, color: '#fff' }}
+    />
+  );
 
-    _renderScene = SceneMap({
-        first: Sence,
-        second: Sence,
-        thirty:Sence
-    });
-    render() {
-        return (
-            <TabViewAnimated
-                navigationState={this.state}
-                renderScene={this._renderScene}
-                renderHeader={this._renderHeader}
-                onIndexChange={this._handleIndexChange}
-                initialLayout={initialLayout}
-            />
-        );
-    }
+  _renderScene = SceneMap({
+    first: Sence,
+    second: Sence,
+    thirty: Sence
+  });
+  render() {
+    return (
+      <TabViewAnimated
+        navigationState={this.state}
+        renderScene={this._renderScene}
+        renderHeader={this._renderHeader}
+        onIndexChange={this._handleIndexChange}
+        initialLayout={initialLayout}
+      />
+    );
+  }
 }
